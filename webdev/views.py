13c798616afb68417c681cb7a6430dcb0fbcb506
Troublemeeter <843519037@qq.com>
2018-11-28 23:37:27 +0800

    Initial commit

---
+++ webdev/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    #return HttpResponse("Hellp World!")
+    return render(request, "index.html")
